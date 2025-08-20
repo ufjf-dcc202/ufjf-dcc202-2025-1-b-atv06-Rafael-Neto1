59ff621c4fa4567ea238774efa0fe31e435bdb69
Rafael Santos da Silva Neto <rafael.neto@estudante.ufjf.br>
2025-08-19 22:48:00 -0300

    Separada a lógica do jogo da interface

--- game.js
+++ game.js
@@ -1,5 +1,4 @@
-let board = [];
-const layoutInicial = [
+export const layoutInicial = [
     [null, null, 1, 1, 1, null, null],
     [null, null, 1, 1, 1, null, null],
     [1, 1, 1, 1, 1, 1, 1],
@@ -9,36 +8,37 @@ const layoutInicial = [
     [null, null, 1, 1, 1, null, null]
 ];
 
-function iniciar() {
-    board = JSON.parse(JSON.stringify(layoutInicial));
+export function criarTabuleiro() {
+    return JSON.parse(JSON.stringify(layoutInicial));
 }
 
-function podeMover(x1, y1, x2, y2) {
-    if (board[y1][x1] !== 1) return false;
-    if (board[y2][x2] !== 0) return false;
+export function podeMover(tabuleiro, x1, y1, x2, y2) {
+    if (tabuleiro[y1][x1] !== 1) return false;
+    if (tabuleiro[y2][x2] !== 0) return false;
 
-    let dx = x2 - x1;
-    let dy = y2 - y1;
+    const dx = x2 - x1;
+    const dy = y2 - y1;
 
     if (Math.abs(dx) === 2 && dy === 0) {
-        let xm = (x1 + x2) / 2;
-        if (board[y1][xm] === 1) return true;
+        const xm = Math.floor((x1 + x2) / 2);
+        return tabuleiro[y1][xm] === 1;
     }
+
     if (Math.abs(dy) === 2 && dx === 0) {
-        let ym = (y1 + y2) / 2;
-        if (board[ym][x1] === 1) return true;
+        const ym = Math.floor((y1 + y2) / 2);
+        return tabuleiro[ym][x1] === 1;
     }
 
     return false;
 }
 
-function mover(x1, y1, x2, y2) {
-    if (podeMover(x1, y1, x2, y2)) {
-        board[y1][x1] = 0;
-        board[y2][x2] = 1;
-        let xm = (x1 + x2) / 2;
-        let ym = (y1 + y2) / 2;
-        board[ym][xm] = 0;
+export function mover(tabuleiro, x1, y1, x2, y2) {
+    if (podeMover(tabuleiro, x1, y1, x2, y2)) {
+        tabuleiro[y1][x1] = 0;
+        tabuleiro[y2][x2] = 1;
+        const xm = Math.floor((x1 + x2) / 2);
+        const ym = Math.floor((y1 + y2) / 2);
+        tabuleiro[ym][xm] = 0;
         return true;
     }
     return false;
